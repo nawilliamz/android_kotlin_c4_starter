23c94fe420709c301035dc41e77f8bb612322249
Nathan Williams <nawilliamz@yahoo.com>
2024-04-10 00:10:08 -0500

    Commit-4: Null pointer expection from findFragmentById() in SelectLocationFragment

--- AndroidStudioProjects/android_kotlin_c4_starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
+++ AndroidStudioProjects/android_kotlin_c4_starter/app/src/main/java/com/udacity/project4/locationreminders/RemindersActivity.kt
@@ -1,25 +1,75 @@
 package com.udacity.project4.locationreminders
 
 import android.os.Bundle
+import android.util.Log
 import android.view.MenuItem
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import androidx.navigation.NavHost
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import com.udacity.project4.R
 import com.udacity.project4.databinding.ActivityRemindersBinding
+import com.udacity.project4.databinding.FragmentSelectLocationBinding
 
 /**
  * The RemindersActivity that holds the reminders fragments
  */
+
+
+
 class RemindersActivity : AppCompatActivity() {
 
+//    lateinit var map: GoogleMap
+
+    companion object {
+
+        fun getGoogleMap(map:GoogleMap):GoogleMap {
+            return map
+        }
+
+    }
+
+
+
     private lateinit var binding: ActivityRemindersBinding
+
+    private lateinit var selectLocationBinding: FragmentSelectLocationBinding
+
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityRemindersBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+
+
+//        val selectLocationView = selectLocationBinding.root
+//        val currentFragOnDisplay = Navigation.findNavController(selectLocationView).currentDestination?.id
+//        if (selectLocationView.id == currentFragOnDisplay) {
+////            selectLocationBinding = DataBindingUtil.inflate(layoutInflater, R.id.map, binding.navHostFragment, false)
+//            val mapFragment = selectLocationView.findViewById<View>(R.id.map).findFragment<Fragment>() as SupportMapFragment
+//            mapFragment.getMapAsync(this)
+//        }
+
+//        val mapFrag = supportFragmentManager.findFragmentById(R.id.map)
+//        Log.i("ReminderActivity", "R.id.map value is ${mapFrag}")
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+//        val mapFragment = mapFrag as SupportMapFragment
+//        mapFragment.getMapAsync(this)
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
@@ -34,6 +84,12 @@ class RemindersActivity : AppCompatActivity() {
         return super.onOptionsItemSelected(item)
     }
 
-
-
+//    override fun onMapReady(googleMap: GoogleMap) {
+//        map = googleMap
+//
+//        // Add a marker in Sydney and move the camera
+//        val sydney = LatLng(-34.0, 151.0)
+//        map.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+//        map.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+//    }
 }
--- AndroidStudioProjects/android_kotlin_c4_starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
+++ AndroidStudioProjects/android_kotlin_c4_starter/app/src/main/java/com/udacity/project4/locationreminders/savereminder/selectreminderlocation/SelectLocationFragment.kt
@@ -1,29 +1,39 @@
 package com.udacity.project4.locationreminders.savereminder.selectreminderlocation
 
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.findFragment
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.udacity.project4.R
 import com.udacity.project4.base.BaseFragment
 import com.udacity.project4.databinding.FragmentSelectLocationBinding
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
 import com.udacity.project4.utils.setDisplayHomeAsUpEnabled
 import org.koin.android.ext.android.inject
 
 class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
 
-    private var  map: GoogleMap? = null
 
-
+    private lateinit var map: GoogleMap
 
     // Use Koin to get the view model of the SaveReminder
     override val _viewModel: SaveReminderViewModel by inject()
     private lateinit var binding: FragmentSelectLocationBinding
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
@@ -31,8 +41,37 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         val layoutId = R.layout.fragment_select_location
         binding = DataBindingUtil.inflate(inflater, layoutId, container, false)
 
+
         binding.viewModel = _viewModel
         binding.lifecycleOwner = this
+
+
+
+        val mapFragXX = this.activity?.supportFragmentManager
+        Log.i("SelectLocationFragment", "The value of mapFragXX is $mapFragXX")
+        val mapFragment = mapFragXX as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+//        val mapFragX = parentFragmentManager.findFragmentById(R.id.map)
+        //This is returned null for mapFragX because this fragment is not associated with a transaction or host
+//        Log.i("SelectLocationFragment", "The value of mapFragX is $mapFragX")
+
+//        val mapFrag = (parentFragment as NavHostFragment).childFragmentManager.findFragmentById(R.id.map)
+        //This is returning null for mapFrag because this fragment (SelectLocationFragment) is attached directly to the
+        //RemindersActivity class (it is not attached/associated with NavHostFragment)
+//        val mapFrag = parentFragmentManager.findFragmentById()
+//        Log.i("SelectLocationFragment", "The value of mapFrag is $mapFrag")
+//        val mapFragment = mapFrag as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+
+
+//        val mapFragY = (parentFragment as NavHostFragment).parentFragmentManager.findFragmentById(R.id.map)
+//        Log.i("SelectLocationFragment", "The value of mapFragY is $mapFragY")
+
+
+//        activity
+//        requireActivity()
+//        Is NavHostFragment associated with an activity or only a context
 
         setHasOptionsMenu(true)
         setDisplayHomeAsUpEnabled(true)
@@ -53,39 +92,40 @@ class SelectLocationFragment : BaseFragment(), OnMapReadyCallback {
         //  and navigate back to the previous fragment to save the reminder and add the geofence
     }
 
+
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater.inflate(R.menu.map_options, menu)
+
+
     }
 
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         // TODO: Change the map type based on the user's selection.
         R.id.normal_map -> {
-            map?.mapType = GoogleMap.MAP_TYPE_NORMAL
+            RemindersActivity.getGoogleMap(map).mapType = GoogleMap.MAP_TYPE_NORMAL
             true
         }
         R.id.hybrid_map -> {
-            map?.mapType = GoogleMap.MAP_TYPE_HYBRID
+            RemindersActivity.getGoogleMap(map).mapType = GoogleMap.MAP_TYPE_HYBRID
             true
         }
         R.id.satellite_map -> {
-            map?.mapType = GoogleMap.MAP_TYPE_SATELLITE
+            RemindersActivity.getGoogleMap(map).mapType = GoogleMap.MAP_TYPE_SATELLITE
             true
         }
         R.id.terrain_map -> {
-            map?.mapType = GoogleMap.MAP_TYPE_TERRAIN
+            RemindersActivity.getGoogleMap(map).mapType = GoogleMap.MAP_TYPE_TERRAIN
             true
         }
         else -> super.onOptionsItemSelected(item)
     }
 
-
     override fun onMapReady(googleMap: GoogleMap) {
 
-        map = googleMap
-
-        // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        map?.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
-        map?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        RemindersActivity.getGoogleMap(map).addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
+        RemindersActivity.getGoogleMap(map).moveCamera(CameraUpdateFactory.newLatLng(sydney))
+
     }
+
 }
